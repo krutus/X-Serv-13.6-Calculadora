e4d3cf7fe90dc30ad64df12790b407b5101c0862
Luis Haro <luisfuenla5@gmail.com>
2015-02-05 12:09:55 +0100

    Actualizado con mas posibles formas de poner la accion

--- calculadora.py
+++ calculadora.py
@@ -21,11 +21,19 @@ if __name__ == "__main__":
     try:
         if sys.argv[1] == "Sumar":
             suma(sys.argv[2], sys.argv[3])
+        if sys.argv[1] == "+":
+            suma(sys.argv[2], sys.argv[3])
         if sys.argv[1] == "Restar":
+            resta(sys.argv[2], sys.argv[3])
+        if sys.argv[1] == "-":
             resta(sys.argv[2], sys.argv[3])
         if sys.argv[1] == "Multiplicar":
             mult(sys.argv[2], sys.argv[3])
+        if sys.argv[1] == "x":
+            mult(sys.argv[2], sys.argv[3])
         if sys.argv[1] == "Dividir":
+            div(sys.argv[2], sys.argv[3])
+        if sys.argv[1] == "/":
             div(sys.argv[2], sys.argv[3])
     except ValueError:
         print("Uno de los valores no es un numero")
